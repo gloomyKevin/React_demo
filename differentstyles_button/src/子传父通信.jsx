85588792bbb628aab9627efccb201c9136edaacc
gloomykevin <1293535916@qq.com>
2020-09-01 20:05:21 +0800

    add 子传父

---
+++ differentstyles_button/src/子传父通信.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Son_to_Father extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
