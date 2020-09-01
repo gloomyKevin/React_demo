d100841ebf3b710aac99b3414c65bdaee43ac5ee
gloomykevin <1293535916@qq.com>
2020-09-01 22:58:50 +0800

    improve

--- differentstyles_button/src/组件通信案例TabControl/Tab.jsx
+++ differentstyles_button/src/组件通信案例TabControl/Tab.jsx
@@ -2,10 +2,19 @@ import React, { Component } from 'react'
 import TabControl from './TabControl'
 
 export default class Tab extends Component {
+    constructor(props) {
+        super(props);
+        this.titles = ['新款', '精选', '流行']
+        this.state = {
+            currentIndex: 0,
+            currentTitle: "新款"
+        }
+    }
+
     render() {
         return (
             <div>
-                <TabControl titles={['新款', '精选', '流行']} />
+                <TabControl />
             </div>
         )
     }
