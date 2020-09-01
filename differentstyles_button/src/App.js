2be4c12b291196a5c9ece5adedf7f6b76b8bb6eb
gloomykevin <1293535916@qq.com>
2020-09-01 20:25:40 +0800

    complete 子传父

--- differentstyles_button/src/App.js
+++ differentstyles_button/src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import './App.css';
 import Button from "./Button"
 import Father_to_son from './父传子通信-属性验证';
+import Son_to_Father from './子传父通信';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         默认按钮
     </Button>
       <Father_to_son />
+      <Son_to_Father />
     </Fragment>
   );
 }
--- differentstyles_button/src/子传父通信.jsx
+++ differentstyles_button/src/子传父通信.jsx
@@ -1,11 +1,36 @@
 import React, { Component } from 'react'
 
 export default class Son_to_Father extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0
+        }
+    }
+
     render() {
+        const { counter } = this.state;
         return (
             <div>
-
+                <h2>当前计数{counter}</h2>
+                {/* <button onClick={e => this.increment()}>+</button> */}
+                <CounterButton btnClick={e => this.increment()} />
             </div>
+        )
+    }
+
+    increment() {
+        this.setState({
+            counter: this.state.counter + 1
+        })
+    }
+}
+
+class CounterButton extends Component {
+    render() {
+        const { btnClick } = this.props;
+        return (
+            <button onClick={btnClick}>+1</button>
         )
     }
 }
