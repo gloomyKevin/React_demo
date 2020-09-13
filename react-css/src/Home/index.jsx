020645d0d5f3e66f77114a78a47faaf7906f066b
gloomykevin <1293535916@qq.com>
2020-09-13 19:44:16 +0800

    improve styled-components

--- react-css/src/Home/index.jsx
+++ react-css/src/Home/index.jsx
@@ -1,18 +1,52 @@
 import React, { PureComponent } from 'react';
 import styled from 'styled-components';
 
+const OwnInput = styled.input.attrs({
+    placeholder: "kevin",
+    bColor: "red"
+})`
+    background-color: lightblue;
+    background-color: ${props => props.bColor};
+    color: ${props => props.color};
+`
 const HomeWrapper = styled.div`
-    font-size:50px;
-    color:red;
+    font-size: 50px;
+    color: red;
+        .banner {
+            background-color: blue;
+        }
+        span {
+            color: #fff;
+
+            &.active {
+            color: #f00
+            }
+
+            &:hover {
+                color: green;
+            }
+
+            &:after {
+                content: "aaa"
+            }
+        }
 `
 
 export default class Home extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: "red"
+        }
+    }
+
     render() {
         return (
             <HomeWrapper>
                 <h2>我是home标题</h2>
                 <div>
                     <span>轮播图</span>
+                    <OwnInput type="text" color={this.state.color} />
                 </div>
             </HomeWrapper>
         )
